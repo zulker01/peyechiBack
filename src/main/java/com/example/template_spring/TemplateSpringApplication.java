b3b05321ce75c192ba680f86c33b2790bd5fc622
zulker01 <zulker02@gmail.com>
2025-09-14 15:30:08 +0600

    pagination public query for homepage created

--- src/main/java/com/example/template_spring/TemplateSpringApplication.java
+++ src/main/java/com/example/template_spring/TemplateSpringApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PeyechiBack {
+public class TemplateSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PeyechiBack.class, args);
+		SpringApplication.run(TemplateSpringApplication.class, args);
 		System.out.println("Hello World test github actions v2 ci cd done");
 	}
 
